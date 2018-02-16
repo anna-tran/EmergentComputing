4cd040a5b0e373e28c9286190edcba268543e364
Anna Home <anna@Annas-Home-Pc.local>
2018-02-16 09:22:04 -0700

    created basic triangle

--- ComputationalOrigami/Assets/MeshScripts/TriangleMesh.cs
+++ ComputationalOrigami/Assets/MeshScripts/TriangleMesh.cs
@@ -11,10 +11,12 @@ public class TriangleMesh : MonoBehaviour {
 
     private void Awake()
     {
-        mesh = GetComponent<MeshFilter>().mesh;
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
     }
     // Use this for initialization
     void Start () {
+    
         MakeMeshData();
         CreateMesh();
 
@@ -26,20 +28,40 @@ public class TriangleMesh : MonoBehaviour {
         vertices = new Vector3[] {
             new Vector3(0,0,0),
             new Vector3(0,0,1),
-            new Vector3(1,0,0)
+            new Vector3(1,0,0),
+            new Vector3(0,0.01f,0),
+            new Vector3(0,0.01f,1),
+            new Vector3(1,0.01f,0)
         };
         // create an array of integers
         // order for Unity to read indices of 'vertices' array
         triangles = new int[] {
             0, 1, 2,    // top face
-            0, 2, 1     // bottom face
+            0, 2, 1,     // bottom face
+            3, 4, 5,
+            5, 4, 3,
+            5, 2, 4,
+            4, 2, 5,
+            4, 1, 2,
+            2, 1, 4,
+            3, 0, 4,
+            4, 0, 3,
+            4, 0, 1,
+            1, 0, 4,
+            5, 3, 2,
+            2, 3, 5,
+            2, 3, 0,
+            0, 2, 3
         };
     }
 
     void CreateMesh()
     {
+
+       
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        
     }
 }
