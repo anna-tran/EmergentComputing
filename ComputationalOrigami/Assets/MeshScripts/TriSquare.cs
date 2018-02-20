88be4deac5f48dfddb2e5a41868756fd30fb6de8
Anna Home <anna@Annas-Home-Pc.local>
2018-02-19 23:06:04 -0700

    paper triangle folding properly

--- ComputationalOrigami/Assets/MeshScripts/TriSquare.cs
+++ ComputationalOrigami/Assets/MeshScripts/TriSquare.cs
@@ -1,27 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TriSquare : MonoBehaviour {
 
-    GameObject triangle1;
-    GameObject triangle2;
+    Triangle[] children;
 
 	// Use this for initialization
 	void Start () {
-        triangle1 = this.gameObject.transform.GetChild(0).gameObject;
-        triangle2 = this.gameObject.transform.GetChild(1).gameObject;
+        int num_children = gameObject.transform.childCount;
+        children = new Triangle[num_children];
+        int num_edges = 0;
+        for (int i = 0; i < num_children; i++)
+        {
+            GameObject child = gameObject.transform.GetChild(i).gameObject;
+            children[i] = child.GetComponent<Triangle>();
+            num_edges = children[i].edges.Count;
 
-        triangle1.transform.Rotate(Vector3.right);
-        print(triangle1.name);
+            children[i].edges.Sort();
+            
+           
+        }
+        for (int i = 0; i < num_edges; i++)
+        {
+            Edge a = children[0].edges.ElementAt(i);
+            Edge b = children[1].edges.ElementAt(i);
+            if (a.Equals(b)) {
+                //print(a);
+            }
+        }
+
+ 
+        
+
+
+
     }
-
+    
     void Update()
     {
-        // Rotate the object around its local X axis at 1 degree per second
-        triangle1.transform.Rotate(Vector3.right * Time.deltaTime);
 
-        // ...also rotate around the World's Y axis
-        triangle1.transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+        children[0].Rotate();
     }
+    
 }
---
+++ ComputationalOrigami/Assets/MeshScripts/Triangle.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Triangle : MonoBehaviour {
+    Mesh mesh;
+    Transform[] pivots;
+    Vector3 direction;
+    public List<Edge> edges { get; set; }
+    bool rotatable;
+    
+
+    private void Awake()
+    {
+
+        edges = new List<Edge>();
+        mesh = GetComponent<MeshFilter>().mesh;
+        int[] tris = mesh.triangles;
+        HashSet<HashSet<int>> uniqueEdges = new HashSet<HashSet<int>>();
+        for (int i = 0; i < tris.Length; i += 3)
+        {
+            HashSet<int> verticeSet = new HashSet<int> { tris[i], tris[i + 1], tris[i + 2] };
+            if (!uniqueEdges.Contains(verticeSet))
+            {
+                uniqueEdges.Add(verticeSet);
+            }
+            
+        }
+
+        foreach (HashSet<int> verticeSet in uniqueEdges)
+        {
+            int[] vertices = new int[3];
+            verticeSet.CopyTo(vertices);
+            int v1 = vertices[0];
+            int v2 = vertices[1];
+            int v3 = vertices[2];
+
+            edges.Add(new Edge(v1, v2));
+            edges.Add(new Edge(v1, v3));
+            edges.Add(new Edge(v2, v3));
+        }
+    }
+
+
+
+    void Start()
+    {
+        rotatable = true;
+        int num_children = gameObject.transform.childCount;
+        pivots = new Transform[num_children];
+        for (int i = 0; i < num_children; i++)
+        {
+            Transform pivot = gameObject.transform.GetChild(i).transform;
+            pivots[i] = pivot;
+        }
+
+
+        direction = transform.TransformDirection(new Vector3(-0.5f, 0, -0.5f));
+    }
+
+    public void Rotate()
+    {
+        if (rotatable)
+        {
+            float degreesPerSecond = 50.0f;
+            transform.RotateAround(pivots[0].position, direction,degreesPerSecond * Time.deltaTime);
+        } 
+       
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.red);
+        }
+
+        string colls = gameObject.name + "\n";
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            ContactPoint contact = collision.contacts[i];
+            Debug.DrawRay(contact.point, contact.normal, Color.red);
+            if (Math.Abs(contact.point.x) < 0.01f &&
+                Math.Abs(contact.point.y) < 0.01f &&
+                Math.Abs(contact.point.z) < 0.01f)
+                
+            {              
+                rotatable = false;
+            }
+        }
+
+    }
+}
